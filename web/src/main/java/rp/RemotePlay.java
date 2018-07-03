2413062e2cf2fcc7fb9137b9503c7089840a0eba
Denys Verizhenko <denys.verizhenko@tech-stack.io>
2018-07-03 12:53:24 +0300

    Initial commit.

---
+++ web/src/main/java/rp/RemotePlay.java
@@ -0,0 +1,12 @@
+package rp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotePlay {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemotePlay.class, args);
+    }
+}
